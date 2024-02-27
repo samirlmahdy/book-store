0a1a0b3e8cbaf9cf42e56ee67dfd8823aa966b82
samirlmahdy <samirlmahdy@gmail.com>
2024-02-27 13:01:18 +0200

    new updates

--- src/components/BooksList.tsx
+++ src/components/BooksList.tsx
@@ -1,7 +1,20 @@
+import useFetch from "../hooks/useFetch";
 import { Book } from "../types";
 import BookComponent from "./Book";
 
-const BooksList = ({ books }: { books: Book[] }) => {
+const BooksList = ({ query }: { query: string }) => {
+  const booksEndPoint = `http://localhost:3030/books`;
+
+  const { data, loading, error } = useFetch<Book[]>(booksEndPoint);
+
+  const books = data?.filter((book) =>
+    book.book_name.toLowerCase().includes(query.toLocaleLowerCase())
+  );
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <p>{error}</p>;
+
   return (
     <div>
       {books?.map((book: Book) => {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,10 @@
 import { useState } from "react";
-import { Book } from "../types";
-import useFetch from "../hooks/useFetch";
 import Search from "../components/Search";
 import BooksList from "../components/BooksList";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const booksEndPoint = `http://localhost:3030/books`;
 
-  const { data, loading, error } = useFetch<Book[]>(booksEndPoint);
-
-  const books = data?.filter((book) =>
-    book.book_name.toLowerCase().includes(query.toLocaleLowerCase())
-  );
-
-  if (error) return <div className="error">{error}</div>;
-
-  if (loading) return <div className="loading">Loading...</div>;
-
-  if (!books) return <div>Kindly Add a book...</div>;
   return (
     <div className="books">
       <h2>Welcome to Books Library</h2>
@@ -26,7 +12,7 @@ const Home = () => {
         query={query}
         setQuery={setQuery}
       />
-      <BooksList books={books} />
+      <BooksList query={query} />
     </div>
   );
 };
